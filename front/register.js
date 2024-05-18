44cfbe7f57eaa08537e3fb7a493b679502c76192
kantipina <ea251591@gmail.com>
2024-05-18 16:07:28 +0200

    registracia

--- front/register.js
+++ front/register.js
@@ -1,5 +1,48 @@
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('registrationForm');
+    const langButtons = document.querySelectorAll('.language-switcher button');
+
+    // Функция для изменения языка
+    function changeLanguage(lang) {
+        const translations = {
+            'en': {
+                'register_account': 'Register Your Account',
+                'name': 'Name',
+                'email': 'Email',
+                'password': 'Password',
+                'show': 'Show',
+                'register': 'Register',
+                'already_have_account': 'Already have an account? Login'
+            },
+            'sk': {
+                'register_account': 'Registrácia účtu',
+                'name': 'Meno',
+                'email': 'Email',
+                'password': 'Heslo',
+                'show': 'Zobraziť',
+                'register': 'Registrovať sa',
+                'already_have_account': 'Už máte účet? Prihlásiť sa'
+            }
+        };
+
+        const elementsToTranslate = document.querySelectorAll('[data-translate]');
+
+        elementsToTranslate.forEach(element => {
+            const translationKey = element.dataset.translate;
+            element.textContent = translations[lang][translationKey];
+        });
+
+        // Установка языка для заголовка страницы
+        document.title = translations[lang]['register_account'];
+    }
+
+    // Обработчики событий для кнопок выбора языка
+    langButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            const lang = this.dataset.lang;
+            changeLanguage(lang); // Вызываем функцию изменения языка при нажатии на кнопку
+        });
+    });
 
     form.addEventListener('submit', async function(event) {
         event.preventDefault();
@@ -48,12 +91,15 @@ document.addEventListener('DOMContentLoaded', function() {
         const togglePassword = document.getElementById('togglePassword');
         if (passwordField.type === 'password') {
             passwordField.type = 'text';
-            togglePassword.textContent = 'Hide';
+            togglePassword.textContent = 'Skryť';
         } else {
             passwordField.type = 'password';
-            togglePassword.textContent = 'Show';
+            togglePassword.textContent = 'Zobraziť';
         }
     }
 
+    // Вызываем функцию togglePasswordVisibility при клике на элемент togglePassword
     document.getElementById('togglePassword').addEventListener('click', togglePasswordVisibility);
+
+    changeLanguage('en'); // Установка английского языка по умолчанию
 });
